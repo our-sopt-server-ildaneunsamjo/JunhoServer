dd92cd2e43d7d4886c599c51165b55a6af308028
junolee <junolee7803@gmail.com>
2020-05-28 20:44:30 +0900

    update user.js

--- homework4/models/user.js
+++ homework4/models/user.js
@@ -1,20 +1,67 @@
-module.exports = [
-    {
-        id: 'gngsn',
-        name: '박경선',
-        password: 'qwerty',
-        email: 'gngsn@gmail.com'
+// module.exports = [
+//     {
+//         id: 'gngsn',
+//         name: '박경선',
+//         password: 'qwerty',
+//         email: 'gngsn@gmail.com'
+//     },
+//     {
+//         id: 'EZYOON',
+//         name: '이지윤',
+//         password: 'fl0wer',
+//         email: 'gngsn@gmail.com'
+//     },
+//     {
+//         id: 'wjdrbs',
+//         name: '최정균',
+//         password: 'password',
+//         email: 'wjdrbs@gmail.com'
+//     }
+// ];
+
+const pool = require('../modules/pool');
+const table = 'user';
+
+const user = {
+    signup: async (id, name, password, salt, email) => {
+        const fields = 'id, name, password, salt, email';
+        const questions = `?, ?, ?, ?, ?`;
+        const values = [id, name, password, salt, email];
+        const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+        try {
+            const result = await pool.queryParamArr(query, values);
+            const insertId = result.insertId;
+            return insertId;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('signup ERROR : ', err.errno, err.code);
+                throw err;
+            }
+            console.log('signup ERROR : ', err);
+            throw err;
+        }
     },
-    {
-        id: 'EZYOON',
-        name: '이지윤',
-        password: 'fl0wer',
-        email: 'gngsn@gmail.com'
+    checkUser: async (id) => {
+        const query = `SELECT * FROM ${table} WHERE id="${id}"`;
+        try {
+            const result = await pool.queryParam(query);
+            if (result.length === 0) {
+                return false;
+            } else return true;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('checkUser ERROR : ', err.errno, err.code);
+                throw err;
+            }
+            console.log('checkUser ERROR : ', err);
+            throw err;
+        }
     },
-    {
-        id: 'wjdrbs',
-        name: '최정균',
-        password: 'password',
-        email: 'wjdrbs@gmail.com'
-    }
-];
+    getUserById: async (id) => {
+        // query문 작성
+        // pool module로 전달해서 결과값 받기
+        // try - catch로 ERROR 받기
+    },
+}
+
+module.exports = user;
